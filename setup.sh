8e349cbba848bde918865ec95f9d26d05797d48f
Tu Dang <huynh.tu.dang@usi.ch>
2016-10-06 13:29:58 +0200

    Add dependencies and update setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+sudo apt-get update -y
+
+sudo apt-get install -y git autoconf python-pip build-essential python-dev
+
 git submodule init
 git submodule update
 
 cd behavioral-model
+sudo ./install_deps.sh
 ./autogen.sh
 ./configure
 sudo make
@@ -14,3 +19,13 @@ sudo pip install -r requirements_v1_1.txt
 sudo python setup.py install
 
 cd ..
+sudo pip install -r requirements.txt
+sudo ./veth_setup.sh
+
+cd pktgen
+mkdir build
+cd build
+cmake ..
+make
+
+cd ../..

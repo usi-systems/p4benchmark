b63376d54512e846a99c1b352564162a2e510695
Tu Dang <huynh.tu.dang@usi.ch>
2016-10-11 17:38:48 +0200

    clean up

--- env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $DIR
-BMV2_PATH=$DIR/behavioral-model
-P4C_BM_PATH=$DIR/p4c-bm
-P4C_BM_SCRIPT=$P4C_BM_PATH/p4c_bm/__main__.py
